77463ab9f0df6decf5ef86380ae3c8300dca5566
Jose Manuel Beauregard Mendez <jm.beauregard26@gmail.com>
2020-06-14 17:55:43 -0500

    add env vars

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify, request
 from github import Github
 from pyquery import PyQuery
+from dotenv import load_dotenv, find_dotenv
+from os import getenv
 import requests
+
+load_dotenv(find_dotenv(), verbose=True)
 
 app = Flask(__name__)
 
@@ -9,6 +13,11 @@ app = Flask(__name__)
 def main():
     return jsonify({"status": "OK", "message": "hello flat"}), 200
 
+port = getenv('PORT')
+gh_key = getenv('GH_KEY')
 
 if __name__ == '__main__':
-    app.run()
+    if gh_key != None:
+		app.run(port=port if port != None else 8000)
+	else:
+		print("Error env GH_KEY missing.")
